e554e9a78afdcb89a1fb52def932c3e5e6fcc885
Than Htut Aung <rday61062@gmail.com>
2024-06-14 21:40:27 +0700

    fixed: yangon time

--- components/utils/time.tsx
+++ components/utils/time.tsx
@@ -12,6 +12,19 @@ export default function Time() {
   }, [])
 
   return (
-    <time dateTime={time.toISOString()}>{format(time, 'Pp')}&#44; YGN</time>
+    <time dateTime={getMyanmarTime(time).toDateString()}>
+      {format(getMyanmarTime(time), 'Pp')}&#44; YGN
+    </time>
   )
+}
+function getMyanmarTime(now: Date) {
+  const utcOffset = 6.5 * 60 // Myanmar is UTC+6:30
+
+  // Convert current time to UTC
+  const utcTime = now.getTime() + now.getTimezoneOffset() * 60000
+
+  // Add Myanmar offset
+  const myanmarTime = new Date(utcTime + utcOffset * 60000)
+
+  return myanmarTime
 }
